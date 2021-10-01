21088c0ca612e5f96a40b80dbc532996735456f0
Riki420 <girottiriccardo1998@gmail.com>
2021-10-01 17:08:05 +0200

    add jumbotron and comics section

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,9 +1,23 @@
 @extends('layouts.main')
+@section('title', 'Comics')
 
 @section('content')
-     <div class="container">
-            <h1 class="text-center">
-                Benvenuto nella Comics Section della <span class="dc-text">DC Comics</span>
-            </h1>
-     </div>
+      <div id="products" class="z-1">
+          <div class="jumbotron"></div>
+        <div class="container">
+            <span class="current">CURRENT SERIES</span>
+            <div class="row">
+                <div class="col-card py-2">
+                    <div class="card mt-2">
+                        <img :src="" alt="comic.series">
+                        <h4>Titolo</h4>
+                        <p>Prezzo</p>
+                    </div> 
+                </div>
+                <div class="me-auto py-2">
+                    <button type="button" id="loadMore" name="loadMore" class="loadMoreBtn">Load More...</button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.main')
-
+@section('title', 'Home')
 @section('content')
      <div class="container">
             <h1 class="text-center">
