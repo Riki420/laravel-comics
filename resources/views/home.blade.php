dda5a7c638876f7d75601bd93a408e62f917aa9a
Riki420 <girottiriccardo1998@gmail.com>
2021-10-01 16:06:30 +0200

    create header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,8 +6,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>DC Comics: Home</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-    
+    <!--Header-->
+    <header>
+        <div class="header-top"></div>
+        <div class="container">
+            <div class="header-bottom">
+                <figure>
+                    <img src="{{ asset('images/dc-logo.png') }}" alt="DC-logo">
+                </figure>
+                <ul class="main-nav">
+                    <li class="me-2">Home</li>
+                    <li>Comics</li>
+                </ul>
+            </div>
+        </div>
+    </header>
 </body>
 </html>
