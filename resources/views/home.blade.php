a99d4036ea446185bfb6309729ec8e4dc1d3f331
Riki420 <girottiriccardo1998@gmail.com>
2021-10-01 16:18:24 +0200

    Init template + include header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,5 @@
+@extends('layouts.main')
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>DC Comics: Home</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-</head>
-<body>
-    <!--Header-->
-    <header>
-        <div class="header-top"></div>
-        <div class="container">
-            <div class="header-bottom">
-                <figure>
-                    <img src="{{ asset('images/dc-logo.png') }}" alt="DC-logo">
-                </figure>
-                <ul class="main-nav">
-                    <li class="me-2">Home</li>
-                    <li>Comics</li>
-                </ul>
-            </div>
-        </div>
-    </header>
-</body>
-</html>
+@section('Content')
+    
+@endsection
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+    <div class="header-top"></div>
+    <div class="container">
+        <div class="header-bottom">
+            <figure>
+                <img src="{{ asset('images/dc-logo.png') }}" alt="DC-logo">
+            </figure>
+            <ul class="main-nav">
+                <li class="me-2"><a href="#">Home</a><li>
+                <li><a href="#">Comics</a></li>
+            </ul>
+        </div>
+    </div>
+</header>
