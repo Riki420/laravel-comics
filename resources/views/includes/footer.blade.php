aaf687308567f46a5557a093c65dd2cd5e6ad973
Riki420 <girottiriccardo1998@gmail.com>
2021-10-01 16:44:07 +0200

    add in main template footer

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+</head>
+<body>
+    <footer>
+    <!--FOOTER TOP-->
+        <div class="footer-top">
+            <div class="container d-flex">
+                    <div class="col">
+                        <h3>DC COMICS</h3>
+                        <ul class="mt-2">
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Video</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul>
+                        <h3 class="mt-2">SHOP</h3>
+                        <ul>
+                            <li><a href="#">Shop DC</a></li>
+                            <li><a href="#">Shop DC Collectiables</a></li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h3>DC</h3>
+                        <ul class="mt-2">
+                            <li><a href="#">Terms of Use</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                            <li><a href="#">Ad Choices</a></li>
+                            <li><a href="#">Adveristing</a></li>
+                            <li><a href="#">Jobs</a></li>
+                            <li><a href="#">Subscriptions</a></li>
+                            <li><a href="#">Talent Workshops</a></li>
+                            <li><a href="#">CPSC Certificates</a></li>
+                            <li><a href="#">Ratings</a></li>
+                            <li><a href="#">Shop Help</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h3>SITES</h3>
+                        <ul class="mt-2">
+                            <li><a href="#">DC</a></li>
+                            <li><a href="#">MAD Magazine</a></li>
+                            <li><a href="#">DC Kids</a></li>
+                            <li><a href="#">DC Universe</a></li>
+                            <li><a href="#">DC Power Vista</a></li>
+                        </ul>
+                    </div>
+                <div class="container-mini">
+                    <figure>
+                        <img src="{{ asset('images/dc-logo-bg.png') }}" class="dc-foot" alt="logo-footer" >
+                    </figure>
+                </div>                
+            </div>
+        </div>
+        <!--FOOTER BOTTOM-->
+        <div class="footer-bottom z-1">
+            <div class="container-foot">
+                <div class="row">
+                    <div class="col-foot">
+                        <!--BOTTONE SIGN-UP-->
+                        <button type="button" id="btn-signup" class="btn-outline-b">Sign-Up Now!</button>
+                        <ul class="none-style d-inline">
+                            <li>
+                                <span class="follow">FOLLOW US:</span>
+                            </li>
+                            <li>
+                                <a href="#"><img src="{{ asset('images/footer-facebook.png') }}" alt="Facebook-logo"></a> 
+                            </li>
+                            <li>
+                                <a href="#"><img src="{{ asset('images/footer-periscope.png') }}" alt="Periscope-logo"></a> 
+                            </li>
+                            <li>
+                                <a href="#"><img src="{{ asset('images/footer-pinterest.png') }}" alt="pinterest-logo"></a> 
+                            </li>
+                            <li>
+                                <a href="#"><img src="{{ asset('images/footer-twitter.png') }}" alt="twitter-logo"></a> 
+                            </li>
+                            <li>
+                                <a href="#"><img src="{{ asset('images/footer-youtube.png') }}" alt="youtube-logo"></a> 
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
